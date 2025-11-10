462c08d1fac8951a051f046a7d3f4595bb7b7173
Legacyarka <legacyxxx03@gmail.com>
2025-11-11 00:38:44 +0530

    saved file

--- index.cpp
+++ index.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
+int main()
+{
     int n = 7;
     bool isprime = true;
-    for (int i = 2; i <n; i++){
-        if(n%i == 0){
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
             isprime = false;
             break;
         }
     }
-    if (isprime == true){
+    if (isprime == true)
+    {
         cout << "the number is prime" << endl;
-    }else{
+    }
+    else
+    {
         cout << "the number is not prime" << endl;
     }
-    
+
     return 0;
 }
