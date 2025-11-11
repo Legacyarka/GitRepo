48f4411c8565456647aff383000a073e478521b0
Legacyarka <legacyxxx03@gmail.com>
2025-11-11 14:10:13 +0530

    inverted triangle pof num

--- index.cpp
+++ index.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-
-    int n = 4;
-    int s = 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= i + 1; j++)
-        {
-            cout << j << " ";
+    int n;
+    cout << "enter the num: ";
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        for (int j = i; j>0; j--){
+            cout << " ";
+        }
+        for(int j = n; j>i; j--){
+            cout << (i+1);
         }
         cout << endl;
     }
