fda9485fd3f6db998550b91295f3021a15e216f6
Legacyarka <legacyxxx03@gmail.com>
2025-11-11 14:57:23 +0530

    pyramid pattern

--- index.cpp
+++ index.cpp
@@ -6,12 +6,30 @@ int main()
     int n;
     cout << "enter the num: ";
     cin >> n;
+    // for(int i = 0; i<n; i++){
+    //     for (int j = 0; j<i; j++){
+    //         cout << " ";
+    //     }
+    //     for(int j = 0; j<(n-i); j++){
+    //         cout << (i+1);
+    //     }
+    //     cout << endl;
+    // }
+
+    // pyramid pattern
+
     for(int i = 0; i<n; i++){
-        for (int j = 0; j<i; j++){
+        int num = 1;
+        int z = 1;
+        for(int j = 0; j<(n-i-1); j++){
             cout << " ";
         }
-        for(int j = 0; j<(n-i); j++){
-            cout << (i+1);
+        for(int j = 0; j<(i+1); j++){
+            cout << num;
+            num++;
+        }
+        for(int j = i; j>0; j--){
+            cout << j;
         }
         cout << endl;
     }
