f893a3b304ffb1236ac44ab5f67e22dac15da388
Legacyarka <legacyxxx03@gmail.com>
2025-11-13 17:46:18 +0530

    sum of digits func

--- index.cpp
+++ index.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cout << "enter the num: ";
-    cin >> n;
-    // for(int i = 0; i<n; i++){
-    //     for (int j = 0; j<i; j++){
-    //         cout << " ";
-    //     }
-    //     for(int j = 0; j<(n-i); j++){
-    //         cout << (i+1);
-    //     }
-    //     cout << endl;
-    // }
 
-    // pyramid pattern
 
-    for(int i = 0; i<n; i++){
-        int num = 1;
-        int z = 1;
-        for(int j = 0; j<(n-i-1); j++){
-            cout << " ";
-        }
-        for(int j = 0; j<(i+1); j++){
-            cout << num;
-            num++;
-        }
-        for(int j = i; j>0; j--){
-            cout << j;
-        }
-        cout << endl;
+int sumofdig(int num){
+    int digitsum = 0;
+    while (num != 0){
+        int rem = num%10;
+        num /= 10;
+        digitsum += rem;
     }
+    return digitsum;
+}
+
+int main(){
+
+    
+    cout << sumofdig(145) << endl;
     return 0;
 }
